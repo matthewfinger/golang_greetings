c11bc94c95f8e9957b81eaaf09408ef389071c89
Matt Finger <matthewfinger3@gmail.com>
2022-12-31 19:17:33 -0600

    version 0.1.1

--- greetings.go
+++ greetings.go
@@ -3,15 +3,36 @@ package golang_greetings
 import (
 	"errors"
 	"fmt"
+	"time"
+	"math/rand"
+	"strings"
 )
+
+func GreetPatterns(liked bool) []string {
+	if (liked) {
+		return []string{
+			"Hello %v, nice to meet you!",
+			"Welcome %v",
+			"%v! Great to see ya",
+		}
+	}
+
+	return []string{
+		"Oh great, it's %v",
+		"%v is here, that means the fun just left",
+		"Boo, you stink",
+	}
+}
+
+// This sets the seed to be precise to the program's start time
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 // Return a greeting for a name based on if the entity is liked
 func Greet(name string, liked bool) string {
-	if (liked) {
-		return fmt.Sprintf("Hello %v, nice to meet you!", name)
-	} else {
-		return fmt.Sprintf("Oh, great, it's %v", name)
-	}
+	patterns := GreetPatterns(liked)
+	return fmt.Sprintf(patterns[rand.Intn(len(patterns))], name)
 }
 
 // returns a greeting for the named person
@@ -19,6 +40,10 @@ func Hello(name string) (string, error) {
 	// if the name is invalid (empty) give an error
 	if (name == "") {
 		return "", errors.New("empty name")
+	}
+
+	if (strings.ToLower(name) == "evan") {
+		return Greet(name, false), nil
 	}
 	return Greet(name, true), nil
 }
